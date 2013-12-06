7139ef281d267423267d07933baeb6a62cd22f86
Denis Mir <mir.denis@googlemail.com>
2013-12-06 22:50:54 +0100

    Added the first hardcoded articles

---
+++ client/views/articles/articles.js
@@ -0,0 +1,18 @@
+var articles = [
+	{
+		title: "Lorem",
+		text: "Lorem dulem."
+	 },
+	 {
+	 	title: "Hello world.",
+	 	text: "And we are this world."
+	 },
+	 {
+	 	title: "Lorem",
+	 	text: "Lorem dulem."
+	 }
+];
+
+Template.articlesList.helpers({
+	articles: articles
+});
