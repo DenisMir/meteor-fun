9390c212521d2fa9283d8b582492c80b251354b4
Denis Mir <mir.denis@googlemail.com>
2013-12-07 13:12:14 +0100

    Rendering articles from the mongo db including some fixtures.

--- client/views/articles/articles.js
+++ client/views/articles/articles.js
@@ -1,18 +1,5 @@
-var articles = [
-	{
-		title: "Lorem",
-		text: "Lorem dulem."
-	 },
-	 {
-	 	title: "Hello world.",
-	 	text: "And we are this world."
-	 },
-	 {
-	 	title: "Lorem",
-	 	text: "Lorem dulem."
-	 }
-];
-
 Template.articlesList.helpers({
-	articles: articles
+	articles: function() {
+		return Articles.find({}, {sort: {date_created: -1}});
+	}
 });
