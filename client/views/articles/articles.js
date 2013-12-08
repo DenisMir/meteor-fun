213dfd8719fe9f58ee47b18ee394dfd139df139a
Denis Mir <mir.denis@googlemail.com>
2013-12-08 21:20:47 +0100

    Allowing to add an article.

--- client/views/articles/articles.js
+++ client/views/articles/articles.js
@@ -3,3 +3,17 @@ Template.articlesList.helpers({
 		return Articles.find({}, {sort: {date_created: -1}});
 	}
 });
+
+Template.postArticle.events({
+	'submit form': function(e) {
+		e.preventDefault();
+		var article = {
+			title: $(e.target).find('[name=title]').val(),
+			text: $(e.target).find('[name=text]').val(),
+			date_created: new Date()
+		};
+
+		article._id = Articles.insert(article);
+		Router.go('article', article);
+	}
+});
