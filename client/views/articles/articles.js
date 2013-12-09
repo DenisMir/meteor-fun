82c165df35e9d220052430871b2bbe8cf5ce258f
Denis Mir <mir.denis@googlemail.com>
2013-12-09 07:30:14 +0100

    Executing the insertion of new articles in a server side method

--- client/views/articles/articles.js
+++ client/views/articles/articles.js
@@ -10,10 +10,14 @@ Template.postArticle.events({
 		var article = {
 			title: $(e.target).find('[name=title]').val(),
 			text: $(e.target).find('[name=text]').val(),
-			date_created: new Date()
 		};
 
-		article._id = Articles.insert(article);
-		Router.go('article', article);
+		Meteor.call('post-article', article, function(error, id) {
+			if(error) {
+				console.log('error happened. ' + error);
+			} else {
+				Router.go('article', {'_id': id});
+			}
+		});
 	}
 });
--- collections/articles.js
+++ collections/articles.js
@@ -1,4 +1,25 @@
 Articles = new Meteor.Collection("articles");
+
+Meteor.methods({
+	'post-article': function(postAttributes) {
+		var user = Meteor.user();
+
+		if(!user) throw new Meteor.Error(401, "You need to be logged in.");
+		if(!postAttributes.title) throw new Meteor.Error(422, "The title is missing.");
+		if(!postAttributes.text) throw new Meteor.Error(422, 'The text is missing.');
+
+		var article = _.extend(_.pick(postAttributes, 'title', 'text'), {
+						userId: user._id,
+						author: user.profile.name,
+						date_created: new Date()
+		});
+
+		var articleId = Articles.insert(article);
+
+		return articleId;
+	}
+})
+
 Articles.allow({
 	insert: function(userId, doc) {
 		return  !! userId;
