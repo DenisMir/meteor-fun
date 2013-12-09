d2ed9b714a44495262df69c97623ef937501ca3a
Denis Mir <mir.denis@googlemail.com>
2013-12-09 07:05:59 +0100

    Showing the access denied message for a user accessing the post-article page

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -1,17 +1,14 @@
 Template.layout.events({
 	"click li#login-google": function(e, tmpl) {
 		Meteor.loginWithGoogle(function(err) {
-			Router.go("articlesList");
 		});
 	},
 	"click li#login-facebook": function(e, tmpl) {
 		Meteor.loginWithFacebook(function(err) {
-			Router.go("articlesList");
 		});
 	},
 	"click a#logout": function(e, tmpl) {
 		Meteor.logout(function(err) {
-			Router.go("articlesList");
 		});
 	}
 });
