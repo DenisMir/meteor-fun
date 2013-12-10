9a1a7d499cf64e556fffa6fff956d8e32e261e16
Denis Mir <mir.denis@googlemail.com>
2013-12-10 14:59:40 +0100

    Updated the login service setup with dummy data. The other data got revoked.

--- server/login_service_setup.js
+++ server/login_service_setup.js
@@ -9,13 +9,13 @@ Meteor.startup(function() {
 
 	Accounts.loginServiceConfiguration.insert({
 	  service: "facebook",
-	  appId: "578214512248467",
-	  secret: "38a5333457fb7f6f6687cd6b216c3d4d"
+	  appId: "<your facebook app id>",
+	  secret: "<your facebook app secret>"
 	});
 
 	Accounts.loginServiceConfiguration.insert({
 	  service: "google",
-	  clientId: "685639966722-seom2bhaiphhlabs64r9h1o1mg8q85da.apps.googleusercontent.com",
-	  secret: "yY8yKUMV4XXYqJd5W4D5icpB"
+	  clientId: "<your google app id>",
+	  secret: "<your google app secret>"
 	});
 });
