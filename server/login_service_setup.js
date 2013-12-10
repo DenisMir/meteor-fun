66ee07bd8fd067e0e02955fbb4f696a95f7c16be
Denis Mir <mir.denis@googlemail.com>
2013-12-10 15:13:16 +0100

    Add the dummy account services entries only when no services are configured.

--- server/login_service_setup.js
+++ server/login_service_setup.js
@@ -1,21 +1,16 @@
 Meteor.startup(function() {
-	Accounts.loginServiceConfiguration.remove({
-	  service: "google"
-	});
 
-	Accounts.loginServiceConfiguration.remove({
-	  service: "facebook"
-	});
+	if(Accounts.loginServiceConfiguration.find().count() == 0) {
+		Accounts.loginServiceConfiguration.insert({
+		  service: "facebook",
+		  appId: "<your facebook app id>",
+		  secret: "<your facebook app secret>"
+		});
 
-	Accounts.loginServiceConfiguration.insert({
-	  service: "facebook",
-	  appId: "<your facebook app id>",
-	  secret: "<your facebook app secret>"
-	});
-
-	Accounts.loginServiceConfiguration.insert({
-	  service: "google",
-	  clientId: "<your google app id>",
-	  secret: "<your google app secret>"
-	});
+		Accounts.loginServiceConfiguration.insert({
+		  service: "google",
+		  clientId: "<your google app id>",
+		  secret: "<your google app secret>"
+		});
+	}
 });
