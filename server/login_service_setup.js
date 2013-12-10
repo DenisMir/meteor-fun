60ba22c8c06d5cf6e187187398bacd0008263fbf
Denis Mir <mir.denis@googlemail.com>
2013-12-10 14:45:04 +0100

    Added the correct google setup for the meteor.denismir.de domain

--- server/login_service_setup.js
+++ server/login_service_setup.js
@@ -15,7 +15,7 @@ Meteor.startup(function() {
 
 	Accounts.loginServiceConfiguration.insert({
 	  service: "google",
-	  clientId: "685639966722.apps.googleusercontent.com",
-	  secret: "AuhTTggCI-EUm1G7v7ITP7TF"
+	  clientId: "685639966722-seom2bhaiphhlabs64r9h1o1mg8q85da.apps.googleusercontent.com",
+	  secret: "yY8yKUMV4XXYqJd5W4D5icpB"
 	});
 });
