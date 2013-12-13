6a015eedfc01d3bdb55a94228dfad4e22e7f996a
Denis Mir <mir.denis@googlemail.com>
2013-12-13 17:56:14 +0100

    Moved the defined remote methods to the server folder

---
+++ server/methods/article_methods.js
@@ -0,0 +1,19 @@
+Meteor.methods({
+	'post-article': function(postAttributes) {
+		var user = Meteor.user();
+
+		if(!isAuthor()) throw new Meteor.Error(401, "You need to be author to post.");
+		if(!postAttributes.title) throw new Meteor.Error(422, "The title is missing.");
+		if(!postAttributes.text) throw new Meteor.Error(422, 'The text is missing.');
+
+		var article = _.extend(_.pick(postAttributes, 'title', 'text'), {
+						userId: user._id,
+						author: user.profile.name,
+						date_created: new Date().getTime()
+		});
+
+		var articleId = Articles.insert(article);
+
+		return articleId;
+	}
+});
